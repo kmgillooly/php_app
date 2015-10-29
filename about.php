26af869b90e97bd546306e06d5c7c399868e3236
Kristina Gillooly <kmgillooly@gmail.com>
2015-10-29 18:36:18 +0000

    trying to commit to new rep

--- about.php
+++ about.php
@@ -8,7 +8,7 @@ include('includes/header.php'); ?>
 	        <div id="bio">
 	        <h3>A little bit about me!</h3>
 			      <img src="img/profilepic.jpg" alt="Photograph of Kristina Gillooly" class="profile-photo">
-			      <p>Hello! This is a portfolio of my work as a painter, photographer, mother, and wife.  I hope to have a career as a web developer.</p>
+			      <p>Hello! This is a sample of my work as a painter, photographer, mother, and wife.  I hope to have a career as a web developer.</p>
 			      <p>I achieved a BA in psychology and art from Case Western Reserve University in Cleveland, Ohio in 2006.  I then completed a MA degree in Graphic Communication from the University of Wolverhampton in 2007 in England.  I worked for a year in England as an applications engineer for a company called Minivator, and as a web designer for a company called IT Futures.  In 2008, I moved back to the United States, and could not find similar work.  After starting a wonderful family, I found Code Louisville and I am back on track with my desired career.</p>
 			      <p>Please follow me on instagram if you would like! My user name is <a href="https://instagram.com/kmgillooly23">@kmgillooly23</a>. </p>
 	        </div>
